bcac7e7b5ef717e5c2fa4d0121e9b0b88b361f2e
Tanmesh Joshi <tanmeshj2002@gamil.com>
2026-03-16 07:25:58 +0530

    util code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+// This is using promises
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req ,res, next)).
+        catch((err)=>next(err))
+    }
+
+}
+
+ 
+
+export {asyncHandler}
+
+// This is using try and catch
+// const asyncHandler = (fn) => async(req , res ,next) => {
+//     try{
+//         await fn(req,res,next)
+
+//     } catch(error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+
