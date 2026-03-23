6adbafb6f9d685c662ab0aa5cd14d16b865d9da8
Tanmesh Joshi <tanmeshj2002@gamil.com>
2026-03-23 19:34:12 +0530

    all_setup config

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 // This is using promises
 const asyncHandler = (requestHandler) => {
-    (req,res,next) => {
+    return (req,res,next) => {
         Promise.resolve(requestHandler(req ,res, next)).
         catch((err)=>next(err))
     }
